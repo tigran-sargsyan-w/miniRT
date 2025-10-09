127034795bfad4a8646c35affdd1036da2a019c5
Extraden <extraden7@gmail.com>
2025-10-09 21:15:19 +0200

    refactor: implement input validation in check_input function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include "miniRT.h"
+#include <fcntl.h>
+#include "libft.h"
 
-int main(void)
+int check_input(int argc, char **argv)
 {
+    int len;
+    if (argc != 2)
+    {
+        printf("Usage: %s <scene_file>\n", argv[0]);
+        return (1);
+    }
+    if (argv[1][0] == '\0')
+    {
+        printf("Error: Empty filename\n");
+        return (1);
+    }
+    len = ft_strlen(argv[1]);
+    if (len < 4 || argv[1][len - 3] != '.' || argv[1][len - 2] != 'r' || argv[1][len - 1] != 't')
+    {
+        printf("Error: Filename must end with .rt\n");
+        return (1);
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    if (check_input(argc, argv) != 0)
+        return (1);
+    if (open(argv[1], O_RDONLY) == -1)
+    {
+        perror("Error opening file");
+        return (1);
+    }
     printf("Hello, miniRT!\n");
     return (0);
 }
