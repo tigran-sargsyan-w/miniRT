13d8a619158cbdc59bc6f1228323a7fdf8c89709
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-17 19:54:13 +0200

    refactor: update vector types and functions to use double precision

--- include/vector.h
+++ include/vector.h
@@ -3,34 +3,34 @@
 
 #define M_PI 3.14159265358979323846
 
-typedef struct s_vector
+typedef struct s_vector3
 {
-    float x;
-    float y;
-    float z;
-} t_vector;
+    double x;
+    double y;
+    double z;
+} t_vector3;
 
-t_vector vec_create(float x, float y, float z);
-t_vector vec_add(t_vector a, t_vector b);
-t_vector vec_sub(t_vector a, t_vector b);
-t_vector vec_scale(t_vector v, float s);
-float vec_dot(t_vector a, t_vector b);
-float vec_length(t_vector v);
-t_vector vec_normalize(t_vector v);
+t_vector3 vec_create(double x, double y, double z);
+t_vector3 vec_add(t_vector3 a, t_vector3 b);
+t_vector3 vec_sub(t_vector3 a, t_vector3 b);
+t_vector3 vec_scale(t_vector3 v, double s);
+double vec_dot(t_vector3 a, t_vector3 b);
+double vec_length(t_vector3 v);
+t_vector3 vec_normalize(t_vector3 v);
 // additional utilities
-t_vector vec_mul(t_vector a, t_vector b); // component-wise multiply
-t_vector vec_div_s(t_vector v, float s);
-float vec_len2(t_vector v);
-t_vector vec_cross(t_vector a, t_vector b);
-int vec_near_zero(t_vector v); // returns 1 if near zero
-t_vector vec_reflect(t_vector v, t_vector n);
+t_vector3 vec_mul(t_vector3 a, t_vector3 b); // component-wise multiply
+t_vector3 vec_div_s(t_vector3 v, double s);
+double vec_len2(t_vector3 v);
+t_vector3 vec_cross(t_vector3 a, t_vector3 b);
+int vec_near_zero(t_vector3 v); // returns 1 if near zero
+t_vector3 vec_reflect(t_vector3 v, t_vector3 n);
 
 // math helpers
-float deg2rad(float deg);
+double deg2rad(double deg);
 
 // robust quadratic solver
 // solves ax^2 + bx + c = 0. returns number of real roots (0,1,2).
 // if roots != NULL it will be filled with up to 2 roots (roots[0] <= roots[1]).
-int solve_quadratic(float a, float b, float c, float roots[2]);
+int solve_quadratic(double a, double b, double c, double roots[2]);
 
 #endif
--- src/vector.c
+++ src/vector.c
@@ -6,121 +6,121 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:47:16 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/10/17 18:21:40 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/10/17 18:45:46 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
 #include <math.h>
 
-t_vector vec_create(float x, float y, float z)
+t_vector3 vec_create(double x, double y, double z)
 {
-    return (t_vector){x, y, z};
+    return (t_vector3){x, y, z};
 }
 
-t_vector vec_add(t_vector a, t_vector b)
+t_vector3 vec_add(t_vector3 a, t_vector3 b)
 {
-    return (t_vector){a.x + b.x, a.y + b.y, a.z + b.z};
+    return (t_vector3){a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
-t_vector vec_sub(t_vector a, t_vector b)
+t_vector3 vec_sub(t_vector3 a, t_vector3 b)
 {
-    return (t_vector){a.x - b.x, a.y - b.y, a.z - b.z};
+    return (t_vector3){a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
-t_vector vec_scale(t_vector v, float s)
+t_vector3 vec_scale(t_vector3 v, double s)
 {
-    return (t_vector){v.x * s, v.y * s, v.z * s};
+    return (t_vector3){v.x * s, v.y * s, v.z * s};
 }
 
-float vec_dot(t_vector a, t_vector b)
+double vec_dot(t_vector3 a, t_vector3 b)
 {
     return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
-float vec_length(t_vector v)
+double vec_length(t_vector3 v)
 {
-    return (sqrtf(v.x * v.x + v.y * v.y + v.z * v.z));
+    return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
 }
 
-t_vector vec_normalize(t_vector v)
+t_vector3 vec_normalize(t_vector3 v)
 {
-    float length;
+    double length;
     
     length = vec_length(v);
     if (length == 0)
-        return (t_vector){0, 0, 0};
-    return vec_scale(v, 1.0f / length);
+        return (t_vector3){0, 0, 0};
+    return vec_scale(v, 1.0 / length);
 }
 
 /* additional utilities */
-t_vector vec_mul(t_vector a, t_vector b)
+t_vector3 vec_mul(t_vector3 a, t_vector3 b)
 {
-    return (t_vector){a.x * b.x, a.y * b.y, a.z * b.z};
+    return (t_vector3){a.x * b.x, a.y * b.y, a.z * b.z};
 }
 
-t_vector vec_div_s(t_vector v, float s)
+t_vector3 vec_div_s(t_vector3 v, double s)
 {
-    if (s == 0.0f)
-        return (t_vector){0, 0, 0};
-    return vec_scale(v, 1.0f / s);
+    if (s == 0.0)
+        return (t_vector3){0, 0, 0};
+    return vec_scale(v, 1.0 / s);
 }
 
-float vec_len2(t_vector v)
+double vec_len2(t_vector3 v)
 {
     return (v.x * v.x + v.y * v.y + v.z * v.z);
 }
 
-t_vector vec_cross(t_vector a, t_vector b)
+t_vector3 vec_cross(t_vector3 a, t_vector3 b)
 {
-    return (t_vector){
+    return (t_vector3){
         a.y * b.z - a.z * b.y,
         a.z * b.x - a.x * b.z,
         a.x * b.y - a.y * b.x
     };
 }
 
-int vec_near_zero(t_vector v)
+int vec_near_zero(t_vector3 v)
 {
-    const float s = 1e-8f;
-    return (fabsf(v.x) < s && fabsf(v.y) < s && fabsf(v.z) < s);
+    const double s = 1e-12;
+    return (fabs(v.x) < s && fabs(v.y) < s && fabs(v.z) < s);
 }
 
-t_vector vec_reflect(t_vector v, t_vector n)
+t_vector3 vec_reflect(t_vector3 v, t_vector3 n)
 {
     // reflect v around normal n: v - 2*dot(v,n)*n
-    float d = vec_dot(v, n);
-    return vec_sub(v, vec_scale(n, 2.0f * d));
+    double d = vec_dot(v, n);
+    return vec_sub(v, vec_scale(n, 2.0 * d));
 }
 
 /* math helpers */
-float deg2rad(float deg)
+double deg2rad(double deg)
 {
-    return (deg * (M_PI / 180.0f));
+    return (deg * (M_PI / 180.0));
 }
 
 /* robust quadratic solver: returns number of real roots (0,1,2).
    Uses numerically stable formulation to reduce catastrophic cancellation. */
-int solve_quadratic(float a, float b, float c, float roots[2])
+int solve_quadratic(double a, double b, double c, double roots[2])
 {
-    if (a == 0.0f)
+    if (a == 0.0)
     {
-        if (b == 0.0f)
+        if (b == 0.0)
             return 0;
         // linear solution bx + c = 0
         if (roots)
             roots[0] = -c / b;
         return 1;
     }
-    float disc = b * b - 4.0f * a * c;
-    if (disc < 0.0f)
+    double disc = b * b - 4.0 * a * c;
+    if (disc < 0.0)
         return 0;
-    float sqrt_disc = sqrtf(disc);
+    double sqrt_disc = sqrt(disc);
     // use numerically stable form
-    float q = -0.5f * (b + (b >= 0.0f ? sqrt_disc : -sqrt_disc));
-    float r1 = q / a;
-    float r2 = c / q;
-    if (disc == 0.0f)
+    double q = -0.5 * (b + (b >= 0.0 ? sqrt_disc : -sqrt_disc));
+    double r1 = q / a;
+    double r2 = c / q;
+    if (disc == 0.0)
     {
         if (roots)
             roots[0] = r1;
