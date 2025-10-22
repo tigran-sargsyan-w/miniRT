f8cfe736b405186f4a1a0d6040234b39778744b8
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-22 22:00:46 +0200

    feat: add ray structure and associated functions for ray tracing

---
+++ include/ray.h
@@ -0,0 +1,14 @@
+#ifndef RAY_H
+# define RAY_H
+# include "vector.h"
+
+typedef struct s_ray
+{
+	t_vector3	orig;
+	t_vector3	dir;
+}	t_ray;
+
+t_ray		ray_make(t_vector3 orig, t_vector3 dir);
+t_vector3	ray_at(const t_ray *r, double t);
+
+#endif
