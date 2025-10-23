b6d81ff565e02055a7ca9d6d85ccbb56d2528473
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-23 10:14:00 +0200

    fix: update parameter name for clarity in ray_at function

--- include/ray.h
+++ include/ray.h
@@ -9,6 +9,6 @@ typedef struct s_ray
 }	t_ray;
 
 t_ray		ray_make(t_vector3 orig, t_vector3 dir);
-t_vector3	ray_at(const t_ray *r, double t);
+t_vector3	ray_at(const t_ray *ray, double distance);
 
 #endif
