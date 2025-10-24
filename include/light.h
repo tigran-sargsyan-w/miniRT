99d681e721832e2c1cf6465f64a4e0a83b0247b2
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-24 19:34:03 +0200

    feat: linked list for obj and lights

--- include/light.h
+++ include/light.h
@@ -1,14 +1,25 @@
 #ifndef LIGHT_H
 # define LIGHT_H
-# include "vector.h"
 # include "color.h"
+# include "vector.h"
 
-typedef struct s_light
+typedef struct s_light		t_light;
+typedef struct s_light_node	t_light_node;
+
+struct						s_light
 {
-	t_vector3		position;
-	t_color			color;     // linear 0..1
-	double			intensity; // 0..∞
-}	t_light;
+	t_vector3				position;
+	t_color color;    // linear 0..1
+	double intensity; // 0..∞
+};
 
-t_light	light_point_make(t_vector3 position, t_color8 color, double intensity);
+// Linked list node for lights
+typedef struct s_light_node
+{
+	t_light					light;
+	struct s_light_node		*next;
+}							t_light_node;
+
+t_light						light_point_make(t_vector3 position, t_color8 color,
+								double intensity);
 #endif
--- include/object.h
+++ include/object.h
@@ -3,7 +3,8 @@
 # include "hit.h"
 # include "ray.h"
 
-typedef struct s_object	t_object;
+typedef struct s_object			t_object;
+typedef struct s_object_node	t_object_node;
 
 // Intersection interface: return 1 if there is a hit in [t_min,t_max].
 typedef int (*t_intersect_func)(const t_object *object,
@@ -17,18 +18,25 @@ typedef enum e_obj_type
 	OBJ_SPHERE = 1,
 	OBJ_PLANE = 2,
 	OBJ_CYL = 3
-}						t_obj_type;
+}								t_obj_type;
 
 // Base class (first field in "derived" structs)
-struct					s_object
+struct							s_object
 {
-	t_obj_type			type;
-	t_intersect_func	intersect_func;
-	t_material			material;
+	t_obj_type					type;
+	t_intersect_func			intersect_func;
+	t_material					material;
 };
 
-void					object_init(t_object *object, t_obj_type type,
-							t_material material,
-							t_intersect_func intersect_func);
+// Linked list node for objects
+struct							s_object_node
+{
+	const t_object *object;
+	struct s_object_node		*next;
+};
+
+void							object_init(t_object *object, t_obj_type type,
+									t_material material,
+									t_intersect_func intersect_func);
 
 #endif
