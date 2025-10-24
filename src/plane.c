7116a31c13a98a304172b8d8790606732b1859c1
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-24 13:19:14 +0200

    feat: add plane structure and initialization function

---
+++ src/plane.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include "plane.h"
+#include "vector.h"
+#include "constants.h"
+
+static int	intersect_plane(const t_object *obj,
+				t_ray ray, double t_min, double t_max, t_hit *out)
+{
+    // TODO: implement ray-plane intersection
+	return (1);
+}
+
+int	plane_init(t_plane *plane, t_vector3 point, t_vector3 normal, t_material material)
+{
+	if (!plane)
+		return (1);
+	if (!vector3_normalize_safe(normal, &normal, RT_EPS))
+		return (1);
+	object_init(&plane->base, OBJ_PLANE, material, &intersect_plane);
+	plane->point = point;
+	plane->normal = normal;
+	return (0);
+}
