cff549a436f7bb155861c3c9283f2555ad3f62a0
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-31 16:21:11 +0100

    refactor: update cylinder structure to replace radius with diameter and adjust initialization parameters

--- include/cylinder.h
+++ include/cylinder.h
@@ -5,13 +5,14 @@
 typedef struct s_cylinder
 {
 	t_object	base;
-	t_vector3	center; //center of the cylinder
-	t_vector3	axis; //unit vector along the axis (direction +Z "local")
-	double		radius;
-	double		half_height;
+	t_vector3	center;
+	t_vector3	orientation;
+	double		diameter;
+	double		height;
+	t_color8	color;
 }	t_cylinder;
 
-int	cylinder_init(t_cylinder *cylinder, t_vector3 center, t_vector3 axis,
-			double radius, double height, t_material material);
+int	cylinder_init(t_cylinder *cylinder, t_vector3 center, t_vector3 orientation,
+			double diameter, double height, t_material material);
 
 #endif
