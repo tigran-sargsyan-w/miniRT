2e844d693c89b230d7d4310857a21b488ef10395
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-10-31 22:12:20 +0100

    refactor: improve formatting of sphere, plane, and cylinder debug print statements

--- src/debug_print_scene.c
+++ src/debug_print_scene.c
@@ -33,7 +33,7 @@ void	debug_print_scene(t_scene *scene)
     for (int i = 0; i < scene->sphere_count; ++i)
     {
         sp = &scene->spheres[i];
-        printf(" Sphere %d:  Center(%.2f, %.2f, %.2f), Diameter: %.2f, Color(%d, %d, %d)\n", i,
+        printf(" Sphere %d: Center(%.2f, %.2f, %.2f), Diameter: %.2f, Color(%d, %d, %d)\n", i,
             sp->center.x, sp->center.y, sp->center.z,
             sp->diameter, sp->color.r, sp->color.g, sp->color.b);
     }
@@ -41,7 +41,7 @@ void	debug_print_scene(t_scene *scene)
     for (int i = 0; i < scene->plane_count; ++i)
     {
         t_plane *pl = &scene->planes[i];
-        printf(" Plane %d:  Point(%.2f, %.2f, %.2f), Normal(%.2f, %.2f, %.2f),  Color(%d, %d, %d)\n", i,
+        printf(" Plane %d: Point(%.2f, %.2f, %.2f), Normal(%.2f, %.2f, %.2f),  Color(%d, %d, %d)\n", i,
             pl->point.x, pl->point.y, pl->point.z,
             pl->normal.x, pl->normal.y, pl->normal.z,
             pl->color.r, pl->color.g, pl->color.b);
@@ -50,7 +50,7 @@ void	debug_print_scene(t_scene *scene)
     for (int i = 0; i < scene->cylinder_count; ++i)
     {
         t_cylinder *cy = &scene->cylinders[i];
-        printf(" Cylinder %d:  Center(%.2f, %.2f, %.2f), Orientation(%.2f, %.2f, %.2f), Diameter: %.2f, Height: %.2f, Color(%d, %d, %d)\n", i,
+        printf(" Cylinder %d: Center(%.2f, %.2f, %.2f), Orientation(%.2f, %.2f, %.2f), Diameter: %.2f, Height: %.2f, Color(%d, %d, %d)\n", i,
             cy->center.x, cy->center.y, cy->center.z,
             cy->orientation.x, cy->orientation.y, cy->orientation.z,
             cy->diameter, cy->height,
