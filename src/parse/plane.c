08048657ffbd5f576c28b69de1d4b9339af42450
dsemenov <dsemenov@student.42lyon.fr>
2025-11-28 21:36:35 +0100

    refactor: Simplify plane parsing by extracting line splitting and component parsing logic

--- src/parse/plane.c
+++ src/parse/plane.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 17:47:10 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/11/28 21:35:19 by dsemenov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "color.h"
 #include "libft.h"
+#include "material.h"
 #include "parse.h"
 #include "plane.h"
-#include "vector.h"
-#include "color.h"
-#include "material.h"
 #include "scene.h"
+#include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -29,45 +41,61 @@ static int	push_plane(t_scene *s, t_plane *pl)
 	return (0);
 }
 
-int	parse_plane(char *line, t_scene *scene)
+static int	split_plane_line(char *line, char ***tab)
 {
-	char	**tab;
-	t_plane	plane;
-	int		n;
-	double	tmp_point[3];
-	double	tmp_normal[3];
-	int		tmp_color[3];
+	int	n;
 
-	tab = ft_split(line, ' ');
-	if (!tab)
+	*tab = ft_split(line, ' ');
+	if (!*tab)
 	{
 		printf("Error\nMemory allocation failed\n");
 		return (1);
 	}
 	n = 0;
-	while (tab[n])
+	while ((*tab)[n])
 		n++;
 	if (n != 3)
 	{
 		printf("Error\nInvalid number of arguments for plane\n");
-		ft_free_tab(tab);
+		ft_free_tab(*tab);
 		return (1);
 	}
-	if (parse_vec3(tab[0], tmp_point) || parse_vec3(tab[1], tmp_normal)
-		|| parse_color(tab[2], tmp_color))
+	return (0);
+}
+
+static int	parse_plane_components(char **tab, double point[3],
+		double normal[3], int color[3])
+{
+	if (parse_vec3(tab[0], point))
+		return (1);
+	if (parse_vec3(tab[1], normal))
+		return (1);
+	if (parse_color(tab[2], color))
+		return (1);
+	return (0);
+}
+
+int	parse_plane(char *line, t_scene *scene)
+{
+	char	**tab;
+	t_plane	plane;
+	double	tmp_point[3];
+	double	tmp_normal[3];
+	int		tmp_color[3];
+
+	if (split_plane_line(line, &tab))
+		return (1);
+	if (parse_plane_components(tab, tmp_point, tmp_normal, tmp_color))
 	{
 		ft_free_tab(tab);
 		return (1);
 	}
 	plane.point = vector3_create(tmp_point[0], tmp_point[1], tmp_point[2]);
 	plane.normal = vector3_create(tmp_normal[0], tmp_normal[1], tmp_normal[2]);
-	plane.color = color8_make((uint8_t)tmp_color[0], (uint8_t)tmp_color[1], (uint8_t)tmp_color[2]);
-	if (plane_init(&plane, plane.point, plane.normal, material_from_rgb8(plane.color)))
-	{
-		ft_free_tab(tab);
-		return (1);
-	}
-	if (push_plane(scene, &plane))
+	plane.color = color8_make((uint8_t)tmp_color[0], (uint8_t)tmp_color[1],
+			(uint8_t)tmp_color[2]);
+	if (plane_init(&plane, plane.point, plane.normal,
+			material_from_rgb8(plane.color)) || push_plane(scene, &plane))
 	{
 		ft_free_tab(tab);
 		return (1);
