a52f24578d98fc1795648a68eccfced2091e69ea
dsemenov <dsemenov@student.42lyon.fr>
2025-11-28 23:40:15 +0100

    refactor: Improve light parsing by extracting position, ratio, and color logic into separate functions

--- src/parse/parse_light.c
+++ src/parse/parse_light.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 17:46:55 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/11/28 22:46:27 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/11/28 23:40:04 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include "parse.h"
 #include "scene.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 static int	split_light_line(char *line, char ***tab, int *size)
 {
@@ -40,25 +39,49 @@ static int	split_light_line(char *line, char ***tab, int *size)
 	return (0);
 }
 
-int	parse_light(char *line, t_scene *scene)
+static int	parse_light_position(t_scene *scene, double pos[3], char **tab)
 {
-	char	**tab;
-	int		size;
-	double	tmp_pos[3];
-	int		col_int[3];
-	char	*endptr;
-	double	val;
-
-	while (*line && ft_strchr(" \t\r\n", *line))
-		line++;
-	if (split_light_line(line, &tab, &size))
-		return (1);
-	if (parse_vec3(tab[0], tmp_pos))
+	if (parse_vec3(tab[0], pos))
 	{
 		ft_free_tab(tab);
 		return (1);
 	}
-	scene->light.position = vector3_create(tmp_pos[0], tmp_pos[1], tmp_pos[2]);
+	scene->light.position = vector3_create(pos[0], pos[1], pos[2]);
+	return (0);
+}
+
+static int	parse_light_ratio(char **tab, double *ratio)
+{
+	char	*endptr;
+	double	val;
+
+	val = ft_strtod(tab[1], &endptr);
+	if (endptr == tab[1])
+	{
+		ft_free_tab(tab);
+		printf("Error\nInvalid light ratio\n");
+		return (1);
+	}
+	if (*endptr != '\0')
+	{
+		ft_free_tab(tab);
+		printf("Error\nInvalid light ratio: trailing characters at '%s'\n",
+			endptr);
+		return (1);
+	}
+	if (val < 0.0 || val > 1.0)
+	{
+		ft_free_tab(tab);
+		printf("Error\nLight ratio out of range (0.0 to 1.0): %f\n", val);
+		return (1);
+	}
+	*ratio = val;
+	return (0);
+}
+
+static int	parse_light_color(t_scene *scene, char **tab, int col_int[3],
+		int size)
+{
 	if (size == 3)
 	{
 		if (parse_color(tab[2], col_int))
@@ -75,24 +98,26 @@ int	parse_light(char *line, t_scene *scene)
 	}
 	scene->light.color = color8_make((uint8_t)col_int[0], (uint8_t)col_int[1],
 			(uint8_t)col_int[2]);
-	{
-		endptr = NULL;
-		val = ft_strtod(tab[1], &endptr);
-		if (endptr == tab[1])
-		{
-			ft_free_tab(tab);
-			printf("Error\nInvalid light intensity\n");
-			return (1);
-		}
-		if (*endptr != '\0')
-		{
-			printf("Error\nInvalid light intensity: trailing characters starting at '%s'\n",
-				endptr);
-			ft_free_tab(tab);
-			return (1);
-		}
-		scene->light.intensity = val;
-	}
+	return (0);
+}
+
+int	parse_light(char *line, t_scene *scene)
+{
+	char	**tab;
+	int		size;
+	double	pos[3];
+	int		col_int[3];
+
+	while (*line && ft_strchr(" \t\r\n", *line))
+		line++;
+	if (split_light_line(line, &tab, &size))
+		return (1);
+	if (parse_light_position(scene, pos, tab))
+		return (1);
+	if (parse_light_ratio(tab, &scene->light.intensity))
+		return (1);
+	if (parse_light_color(scene, tab, col_int, size))
+		return (1);
 	ft_free_tab(tab);
 	return (0);
 }
