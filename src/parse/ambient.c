2ec51fdc9b7f45990d913545f4562b5fc4d12251
dsemenov <dsemenov@student.42lyon.fr>
2025-11-28 17:36:21 +0100

    refactor: Improve ambient parsing by extracting line splitting logic into a separate function

--- src/parse/ambient.c
+++ src/parse/ambient.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ambient.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 17:30:03 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/11/28 17:35:40 by dsemenov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "parse.h"
 #include <stdio.h>
@@ -13,39 +25,47 @@ int	parse_ratio(char *str, double *ratio)
 		printf("Error\nInvalid double value for ambient ratio\n");
 		return (1);
 	}
-	// Reject any trailing junk characters after the numeric part
-	if (*endptr != '\0') 
+	if (*endptr != '\0')
 	{
+		printf("Error\nInvalid trailing characters in ambient ratio\n");
 		return (1);
 	}
 	*ratio = value;
 	return (0);
 }
 
-int	parse_ambient(char *line, t_scene *scene)
+static int	split_ambient_line(char *line, char ***tab)
 {
-	char	**tab;
-	int			i;
-	double		ratio_tmp;
-	int			tmp_color[3];
+	int	n;
 
-	while (*line && ft_strchr(" \t\r\n", *line))
-		line++;
-	tab = ft_split(line, ' ');
-	if (!tab)
+	*tab = ft_split(line, ' ');
+	if (!*tab)
 	{
 		printf("Error\nMemory allocation failed\n");
 		return (1);
 	}
-	i = 0;
-	while (tab[i])
-		i++;
-	if (i != 2)
+	n = 0;
+	while ((*tab)[n])
+		n++;
+	if (n != 2)
 	{
-		printf("Error\nAmbient light requires 2 parameters\n");
-		ft_free_tab(tab);
+		printf("Error\nAmbient light: expected 2 parameters, got %d\n", n);
+		ft_free_tab(*tab);
 		return (1);
 	}
+	return (0);
+}
+
+int	parse_ambient(char *line, t_scene *scene)
+{
+	char	**tab;
+	double	ratio_tmp;
+	int		tmp_color[3];
+
+	while (*line && ft_strchr(" \t\r\n", *line))
+		line++;
+	if (split_ambient_line(line, &tab))
+		return (1);
 	if (parse_ratio(tab[0], &ratio_tmp))
 	{
 		printf("Error\nAmbient light ratio parsing failed\n");
@@ -58,7 +78,8 @@ int	parse_ambient(char *line, t_scene *scene)
 		return (1);
 	}
 	scene->ambient.intensity = ratio_tmp;
-	scene->ambient.color = color8_make((uint8_t)tmp_color[0], (uint8_t)tmp_color[1], (uint8_t)tmp_color[2]);
+	scene->ambient.color = color8_make((uint8_t)tmp_color[0],
+			(uint8_t)tmp_color[1], (uint8_t)tmp_color[2]);
 	ft_free_tab(tab);
 	return (0);
 }
