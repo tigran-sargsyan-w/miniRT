c5018e95a3e8cea22c33ead2a4a6f109a308a3b7
dsemenov <dsemenov@student.42lyon.fr>
2025-12-01 19:35:52 +0100

    refactor: Improve sphere parsing with enhanced error handling and memory management

--- src/parse/parse_sphere.c
+++ src/parse/parse_sphere.c
@@ -6,33 +6,36 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 17:47:15 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/11/28 22:46:13 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/12/01 19:34:53 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "color.h"
 #include "libft.h"
+#include "material.h"
 #include "parse.h"
 #include "sphere.h"
 #include "vector.h"
-#include "color.h"
-#include "material.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-static int	spheres_push(t_scene *s, t_sphere *sp)
+static int	sphere_push(t_scene *s, t_sphere *sp)
 {
 	t_sphere	*arr;
-    int         i;
+	int			i;
 
 	arr = malloc(sizeof(*arr) * (s->sphere_count + 1));
 	if (!arr)
+	{
+		printf("Error\nMemory allocation failed\n");
 		return (1);
-    i = 0;
-    while (i < s->sphere_count)
-    {
-        arr[i] = s->spheres[i];
-        i++;
-    }
+	}
+	i = 0;
+	while (i < s->sphere_count)
+	{
+		arr[i] = s->spheres[i];
+		i++;
+	}
 	arr[s->sphere_count] = *sp;
 	free(s->spheres);
 	s->spheres = arr;
@@ -40,57 +43,77 @@ static int	spheres_push(t_scene *s, t_sphere *sp)
 	return (0);
 }
 
+static int	split_sphere_line(char *line, char ***tab)
+{
+	int	n;
+
+	*tab = ft_split(line, ' ');
+	if (!*tab)
+	{
+		printf("Error\nMemory allocation failed\n");
+		return (1);
+	}
+	n = 0;
+	while ((*tab)[n])
+		n++;
+	if (n != 3)
+	{
+		printf("Error\nSphere: expected 3 parameters, got %d\n", n);
+		ft_free_tab(*tab);
+		return (1);
+	}
+	return (0);
+}
+
+static int	parse_sphere_diameter(char **tab, double *diameter)
+{
+	char	*endptr;
+	double	val;
+
+	val = ft_strtod(tab[1], &endptr);
+	if (endptr == tab[1])
+	{
+		ft_free_tab(tab);
+		printf("Error\nInvalid double value for sphere diameter\n");
+		return (1);
+	}
+	if (*endptr != '\0')
+	{
+		ft_free_tab(tab);
+		printf("Error\nInvalid trailing characters in sphere diameter\n");
+		return (1);
+	}
+	if (val <= 0.0)
+	{
+		ft_free_tab(tab);
+		printf("Error\nSphere diameter must be positive\n");
+		return (1);
+	}
+	*diameter = val;
+	return (0);
+}
+
 int	parse_sphere(char *line, t_scene *scene)
 {
-	int			n;
 	char		**tab;
 	t_sphere	sp;
 	double		tmp_center[3];
 	int			tmp_color[3];
 
-	tab = ft_split(line, ' ');
-	if (!tab)
+	if (split_sphere_line(line, &tab))
 		return (1);
-	n = 0;
-	while (tab[n])
-		n++;
-	if (n != 3)
-	{
-		ft_free_tab(tab);
-		printf("Error\nInvalid number of parameters for sphere\n");
-		return (1);
-	}
 	if (parse_vec3(tab[0], tmp_center) || parse_color(tab[2], tmp_color))
 	{
 		ft_free_tab(tab);
 		return (1);
 	}
 	sp.center = vector3_create(tmp_center[0], tmp_center[1], tmp_center[2]);
-	sp.color = color8_make((uint8_t)tmp_color[0], (uint8_t)tmp_color[1], (uint8_t)tmp_color[2]);
-	{
-		char *endptr = NULL;
-		double val = ft_strtod(tab[1], &endptr);
-		if (endptr == tab[1])
-		{
-			ft_free_tab(tab);
-			printf("Error\nInvalid sphere diameter\n");
-			return (1);
-		}
-		if (*endptr != '\0')
-		{
-			printf("Error\nInvalid sphere diameter: trailing characters starting at '%s'\n", endptr);
-			ft_free_tab(tab);
-			return (1);
-		}
-		sp.diameter = val;
-	}
-	// initialize material/object base
-	if (sphere_init(&sp, sp.center, sp.diameter, material_from_rgb8(sp.color)))
-	{
-		ft_free_tab(tab);
+	sp.color = color8_make((uint8_t)tmp_color[0], (uint8_t)tmp_color[1],
+			(uint8_t)tmp_color[2]);
+	if (parse_sphere_diameter(tab, &sp.diameter))
 		return (1);
-	}
-	if (spheres_push(scene, &sp))
+	if (sphere_init(&sp, sp.center, sp.diameter, material_from_rgb8(sp.color))
+		|| sphere_push(scene, &sp))
 	{
 		ft_free_tab(tab);
 		return (1);
