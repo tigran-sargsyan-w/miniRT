de8c903c741270d5901f6691b2388b8ebd420517
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-02 21:02:28 +0100

    refactor: Enhance documentation for rotation functions and add description for Euler rotation

--- src/transform.c
+++ src/transform.c
@@ -6,13 +6,19 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 19:21:15 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/12/02 19:21:17 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/12/02 21:02:00 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "transform.h"
 
+/**
+ * @brief Applies rotation around X axis (pitch)
+ * @param v - pointer to vector to rotate
+ * @param cx - cos(angle)
+ * @param sx - sin(angle)
+ */
 static void	apply_rx(t_vector3 *v, double cx, double sx)
 {
 	double	tmp_y;
@@ -22,6 +28,12 @@ static void	apply_rx(t_vector3 *v, double cx, double sx)
 	v->z = tmp_y * sx + v->z * cx;
 }
 
+/**
+ * @brief Applies rotation around Y axis (yaw)
+ * @param v - pointer to vector to rotate
+ * @param cy - cos(angle)
+ * @param sy - sin(angle)
+ */
 static void	apply_ry(t_vector3 *v, double cy, double sy)
 {
 	double	tmp_x;
@@ -31,6 +43,12 @@ static void	apply_ry(t_vector3 *v, double cy, double sy)
 	v->z = -tmp_x * sy + v->z * cy;
 }
 
+/**
+ * @brief Applies rotation around Z axis (roll)
+ * @param v - pointer to vector to rotate
+ * @param cz - cos(angle)
+ * @param sz - sin(angle)
+ */
 static void	apply_rz(t_vector3 *v, double cz, double sz)
 {
 	double	tmp_x;
@@ -40,6 +58,15 @@ static void	apply_rz(t_vector3 *v, double cz, double sz)
 	v->y = tmp_x * sz + v->y * cz;
 }
 
+/**
+ * @brief Rotates vector by Euler angles (XYZ order)
+ * Used for rotating object orientations and normals
+ * @param v - vector to rotate
+ * @param rx - rotation around X axis in radians
+ * @param ry - rotation around Y axis in radians
+ * @param rz - rotation around Z axis in radians
+ * @return t_vector3 - rotated vector
+ */
 t_vector3	rotate_euler_vec(t_vector3 v, double rx, double ry, double rz)
 {
 	apply_rx(&v, cos(rx), sin(rx));
