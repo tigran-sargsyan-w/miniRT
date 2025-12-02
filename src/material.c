3dca0d0dd8943dd1b7a3d3c40a7bb22e713c2ae8
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-02 22:54:17 +0100

    refactor: Update formatting and add missing newline at end of material.c

--- src/material.c
+++ src/material.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   material.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 22:53:31 by tsargsya          #+#    #+#             */
+/*   Updated: 2025/12/02 22:53:56 by tsargsya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <material.h>
 
 t_material	material_from_rgb8(t_color8 rgb)
 {
-	t_material material;
+	t_material	material;
 
 	material.base_color = color_from_rgb8(rgb);
 	return (material);
