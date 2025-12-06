2f4d78d85d4b59931a41e135c94491f7b49819f1
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-06 22:57:27 +0100

    refactor: format constants.h

--- include/constants.h
+++ include/constants.h
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsemenov <dsemenov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 23:46:42 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/12/01 23:46:42 by dsemenov         ###   ########lyon.fr   */
+/*   Updated: 2025/12/06 22:55:45 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
-#  ifndef M_PI
-#   define M_PI 3.14159265358979323846
-#  endif
+# ifndef M_PI
+#  define M_PI 3.14159265358979323846
+# endif
 
-#  ifndef RT_EPS
-#   define RT_EPS 1e-9
-#  endif
+# ifndef RT_EPS
+#  define RT_EPS 1e-9
+# endif
 
 # ifndef RT_MIN_OBJECT_EXTENT
 #  define RT_MIN_OBJECT_EXTENT 1e-6
@@ -42,78 +42,78 @@
 # endif
 
 # ifndef KeyEscape
-# define KeyEscape 65307
+#  define KeyEscape 65307
 # endif
 
 // Optional convenient movement keys (X11 KeySyms)
 # ifndef KeyLeft
-# define KeyLeft 65361
+#  define KeyLeft 65361
 # endif
 # ifndef KeyUp
-# define KeyUp 65362
+#  define KeyUp 65362
 # endif
 # ifndef KeyRight
-# define KeyRight 65363
+#  define KeyRight 65363
 # endif
 # ifndef KeyDown
-# define KeyDown 65364
+#  define KeyDown 65364
 # endif
 # ifndef KeyPageUp
-# define KeyPageUp 65365
+#  define KeyPageUp 65365
 # endif
 # ifndef KeyPageDown
-# define KeyPageDown 65366
+#  define KeyPageDown 65366
 # endif
 
 // WASDQE for 3D translation
 # ifndef KeyW
-# define KeyW 119
+#  define KeyW 119
 # endif
 # ifndef KeyA
-# define KeyA 97
+#  define KeyA 97
 # endif
 # ifndef KeyS
-# define KeyS 115
+#  define KeyS 115
 # endif
 # ifndef KeyD
-# define KeyD 100
+#  define KeyD 100
 # endif
 # ifndef KeyQ
-# define KeyQ 113
+#  define KeyQ 113
 # endif
 # ifndef KeyE
-# define KeyE 101
+#  define KeyE 101
 # endif
 
 // Rotations: I/K around X, J/L around Y, U/O around Z
 # ifndef KeyI
-# define KeyI 105
+#  define KeyI 105
 # endif
 # ifndef KeyK
-# define KeyK 107
+#  define KeyK 107
 # endif
 # ifndef KeyJ
-# define KeyJ 106
+#  define KeyJ 106
 # endif
 # ifndef KeyL
-# define KeyL 108
+#  define KeyL 108
 # endif
 # ifndef KeyU
-# define KeyU 117
+#  define KeyU 117
 # endif
 # ifndef KeyO
-# define KeyO 111
+#  define KeyO 111
 # endif
 
 // Transform steps
 # ifndef MOVE_STEP
-# define MOVE_STEP 0.25
+#  define MOVE_STEP 0.25
 # endif
 # ifndef ROTATE_STEP_DEG
-# define ROTATE_STEP_DEG 5.0
+#  define ROTATE_STEP_DEG 5.0
 # endif
 # ifndef SCALE_STEP
-# define SCALE_STEP 1.05
+#  define SCALE_STEP 1.05
 # endif
 
-# endif
+#endif
