4527a216b2eec48d03283cf4e0407580da10f7cc
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-06 22:43:24 +0100

    refactor: enhance scene debug printing functions for clarity and structure

--- src/debug_print_scene.c
+++ src/debug_print_scene.c
@@ -1,59 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug_print_scene.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/06 21:52:55 by tsargsya          #+#    #+#             */
+/*   Updated: 2025/12/06 22:40:05 by tsargsya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "scene.h"
 #include <stdio.h>
 
-void	debug_print_scene(t_scene *scene)
+void	print_scene_objects(t_scene *s)
 {
+	if (s->has_ambient)
+		printf("Ambient Light: %d\n Inten: %.2f Col: (%d, %d, %d)\n",
+			s->has_ambient, s->ambient.intensity,
+			s->ambient.color.r, s->ambient.color.g, s->ambient.color.b);
+	if (s->has_camera)
+		printf("Camera: %d\n Pos: (%.2f, %.2f, %.2f) Ori: (%.2f, %.2f, %.2f)"
+			" FOV: %d\n", s->has_camera,
+			s->camera.position.x, s->camera.position.y, s->camera.position.z,
+			s->camera.orientation.x, s->camera.orientation.y,
+			s->camera.orientation.z, s->camera.fov);
+	if (s->has_light)
+		printf("Light: %d\n Pos: (%.2f, %.2f, %.2f) Col: (%d, %d, %d)"
+			" Inten: %.2f\n", s->has_light,
+			s->light.position.x, s->light.position.y, s->light.position.z,
+			s->light.color.r, s->light.color.g, s->light.color.b,
+			s->light.intensity);
+}
+
+void	print_spheres(t_scene *scene)
+{
+	int			i;
 	t_sphere	*sp;
 
-	printf("Scene Debug Info:\n");
-	printf("Ambient Light: %d\n", scene->has_ambient);
-    if (scene->has_ambient)
-    {
-        printf(" Intensity: %.2f Color: (%d, %d, %d)\n",
-            scene->ambient.intensity,
-            scene->ambient.color.r, scene->ambient.color.g, scene->ambient.color.b);
-    }
-    printf("Camera: %d\n", scene->has_camera);
-    if (scene->has_camera)
-    {
-        printf(" Position: (%.2f, %.2f, %.2f) Orientation: (%.2f, %.2f, %.2f) FOV: %d\n",
-            scene->camera.position.x, scene->camera.position.y, scene->camera.position.z,
-            scene->camera.orientation.x, scene->camera.orientation.y, scene->camera.orientation.z,
-            scene->camera.fov);
-    }
-    printf("Light: %d\n", scene->has_light);
-    if (scene->has_light)
-    {
-        printf("Position: (%.2f, %.2f, %.2f) Color: (%d, %d, %d) Intensity: %.2f\n",
-            scene->light.position.x, scene->light.position.y, scene->light.position.z,
-            scene->light.color.r, scene->light.color.g, scene->light.color.b,
-            scene->light.intensity);
-    }
+	i = 0;
 	printf("Number of Spheres: %d\n", scene->sphere_count);
-    for (int i = 0; i < scene->sphere_count; ++i)
-    {
-        sp = &scene->spheres[i];
-        printf(" Sphere %d: Center(%.2f, %.2f, %.2f), Diameter: %.2f, Color(%d, %d, %d)\n", i,
-            sp->center.x, sp->center.y, sp->center.z,
-            sp->diameter, sp->color.r, sp->color.g, sp->color.b);
-    }
-    printf("Number of Planes: %d\n", scene->plane_count);
-    for (int i = 0; i < scene->plane_count; ++i)
-    {
-        t_plane *pl = &scene->planes[i];
-        printf(" Plane %d: Point(%.2f, %.2f, %.2f), Normal(%.2f, %.2f, %.2f),  Color(%d, %d, %d)\n", i,
-            pl->point.x, pl->point.y, pl->point.z,
-            pl->normal.x, pl->normal.y, pl->normal.z,
-            pl->color.r, pl->color.g, pl->color.b);
-    }
-    printf("Number of Cylinders: %d\n", scene->cylinder_count);
-    for (int i = 0; i < scene->cylinder_count; ++i)
-    {
-        t_cylinder *cy = &scene->cylinders[i];
-        printf(" Cylinder %d: Center(%.2f, %.2f, %.2f), Orientation(%.2f, %.2f, %.2f), Diameter: %.2f, Height: %.2f, Color(%d, %d, %d)\n", i,
-            cy->center.x, cy->center.y, cy->center.z,
-            cy->orientation.x, cy->orientation.y, cy->orientation.z,
-            cy->diameter, cy->height,
-            cy->color.r, cy->color.g, cy->color.b);
-    }
+	while (i < scene->sphere_count)
+	{
+		sp = &scene->spheres[i];
+		printf(" Sphere %d: Center(%.2f, %.2f, %.2f),"
+			" Diameter: %.2f, Color(%d, %d, %d)\n",
+			i,
+			sp->center.x, sp->center.y, sp->center.z,
+			sp->diameter,
+			sp->color.r, sp->color.g, sp->color.b);
+		i++;
+	}
+}
+
+void	print_planes(t_scene *scene)
+{
+	int			i;
+	t_plane		*pl;
+
+	i = 0;
+	printf("Number of Planes: %d\n", scene->plane_count);
+	while (i < scene->plane_count)
+	{
+		pl = &scene->planes[i];
+		printf(" Plane %d: Point(%.2f, %.2f, %.2f),"
+			" Normal(%.2f, %.2f, %.2f), Color(%d, %d, %d)\n",
+			i,
+			pl->point.x, pl->point.y, pl->point.z,
+			pl->normal.x, pl->normal.y, pl->normal.z,
+			pl->color.r, pl->color.g, pl->color.b);
+		i++;
+	}
+}
+
+void	print_cylinders(t_scene *scene)
+{
+	int			i;
+	t_cylinder	*cy;
+
+	i = 0;
+	printf("Number of Cylinders: %d\n", scene->cylinder_count);
+	while (i < scene->cylinder_count)
+	{
+		cy = &scene->cylinders[i];
+		printf(" Cylinder %d: Center(%.2f, %.2f, %.2f),"
+			" Orientation(%.2f, %.2f, %.2f),"
+			" Diameter: %.2f, Height: %.2f, Color(%d, %d, %d)\n",
+			i,
+			cy->center.x, cy->center.y, cy->center.z,
+			cy->orientation.x, cy->orientation.y, cy->orientation.z,
+			cy->diameter, cy->height,
+			cy->color.r, cy->color.g, cy->color.b);
+		i++;
+	}
+}
+
+void	debug_print_scene(t_scene *scene)
+{
+	printf("Scene Debug Info:\n");
+	print_scene_objects(scene);
+	print_spheres(scene);
+	print_planes(scene);
+	print_cylinders(scene);
 }
